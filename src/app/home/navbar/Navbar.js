df3d60c9f487791c5490e84b95ec161538322dcc
bedew <sunhe1007@126.com>
2018-09-11 17:22:23 -0400

    added sidebar

--- src/app/home/navbar/Navbar.js
+++ src/app/home/navbar/Navbar.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from "react-redux";
+import { withRouter } from 'react-router';
 import { Observable } from 'rxjs/Observable';
+import BurgerMenu from '../../common/ui/burger/BurgerMenu';
 
 import './Navbar.css';
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { collapse: false };
+    this.state = {
+      collapse: false,
+      sidebarOpen: false
+    };
   }
 
   componentDidMount() {
@@ -27,13 +33,11 @@ class Navbar extends Component {
   }
 
   render() {
-    let { collapse } = this.state;
+    let { viewportType } = this.props;
+    let { collapse, sidebarOpen } = this.state;
 
-    return (
-      <div className={`Navbar font-primary ${collapse ? 'collapse' : ''}`}>
-        <div className="logo">
-          <a className="logo-anchor" href="#">litemessage</a>
-        </div>
+    let tabs = (
+      <Fragment>
         <div className="tab">
           <i className="fab fa-connectdevelop"></i>
           <span>Peers</span>
@@ -50,9 +54,29 @@ class Navbar extends Component {
           <i className="fas fa-question-circle"></i>
           <span>About</span>
         </div>
+      </Fragment>
+    );
+
+    return (
+      <div className={`Navbar font-primary ${collapse ? 'collapse' : ''}`}>
+        <div className="logo">
+          <a className="logo-anchor" href="#">litemessage</a>
+        </div>
+        {viewportType === 'VIEWPORT_DESKTOP' ? tabs : (
+          <BurgerMenu open={sidebarOpen}
+            onMenuIconClick={() => this.setState({ sidebarOpen: true })}
+            onCloseIconClick={() => this.setState({ sidebarOpen: false })}>
+            {tabs}
+          </BurgerMenu>
+        )}
       </div>
     );
   }
 }
 
-export default Navbar;
+export default withRouter(connect(
+  state => ({
+    viewportType: state.ui.viewportType,
+  }),
+  dispatch => ({})
+)(Navbar));
