4f4522f8d589fca77883b712112edbe8bebe3661
bedew <sunhe1007@126.com>
2018-10-10 23:44:16 -0400

    completed the timeout overlay for pending message card

--- src/app/home/pending-msg/PendingMsg.js
+++ src/app/home/pending-msg/PendingMsg.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { withRouter } from 'react-router';
 import LitemsgForm from '../litemsg-form/LitemsgForm';
 import Litemessage from '../litemessage/Litemessage';
 import DotSpinnerVar1 from '../../common/ui/dot-spinner-var1/DotSpinnerVar1';
+import ButtonLink from '../../common/ui/buttons/button-link/ButtonLink';
 
+import { resetMsg } from '../../common/state/litemsgs/index';
 import './PendingMsg.css';
 
 class PendingMsg extends Component {
+  handleResendBtnClick = () => {
+    let { pendingId, resetMsg } = this.props;
+
+    resetMsg(pendingId);
+  };
+
   render() {
     let { litemsg, sentAt, timeout, blockId } = this.props;
     let body = null, msgState = null;
@@ -29,17 +39,21 @@ class PendingMsg extends Component {
     return (
       <div className={`PendingMsg ${msgState || ''}`}>
         {body}
-        <div className="PendingMsg-overlay">
+        <div className="PendingMsg-overlay text-color-primary">
           {/* waiting overlay */}
           {msgState === 'msg-waiting' && (
             <DotSpinnerVar1 
-              className="PendingMsg-waiting-spinner text-color-primary"
+              className="PendingMsg-waiting-spinner"
               text="Waiting peers to mine the next block that contains this litemessage. It could take from few seconds to a couple of minutes." />
           )}
 
           {/* timeout overlay */}
           {msgState === 'msg-timeout' && (
-            'timeout...'
+            <div>
+              It timeouts. Maybe it got lost in the network, 
+              or the block is forked off the main branch.&nbsp;
+              <ButtonLink onClick={this.handleResendBtnClick}>Try resend.</ButtonLink>
+            </div>
           )}
 
           {/* success overlay */}
@@ -52,4 +66,11 @@ class PendingMsg extends Component {
   }
 }
 
-export default PendingMsg;
+export default withRouter(connect(
+  state => ({}),
+  dispatch => ({
+    resetMsg(pendingId) {
+      dispatch(resetMsg(pendingId));
+    }
+  })
+)(PendingMsg));
